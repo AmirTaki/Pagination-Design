e636b6befd8568cfba3e57d4a5813a891452a0d8
amir <amirtaki623@gmail.com>
2025-10-02 20:23:51 +0330

    complete project

--- src/main.js
+++ src/main.js
@@ -1,70 +1,64 @@
-const element = document.querySelector('.pagination ul')
-
+const element = document.querySelector(".pagination ul");
 let totalPages = 20;
 let page = 10;
 
-
+element.innerHTML = createPagination(totalPages, page);
 const  createPagination = (totalPages, page) => {
     let liTag = '';
     let active;
     let beforePage = page - 1;
-    let afterPag = page + 1;
-    if (page > 1){
-        liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>` 
+    let afterPage = page + 1;
+    
+    if(page > 1){ 
+        liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
     }
-
-    if(page > 2) {
-        liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`
-        
-        if(page > 3){
-            liTag += `<li class="dots"><span>...</span></li>`
+    if(page > 2){
+        liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
+        if(page > 3){ 
+        liTag += `<li class="dots"><span>...</span></li>`;
         }
     }
 
-    if(page == totalPages){
+    if (page == totalPages) {
         beforePage = beforePage - 2;
-    }
-    else if (page == totalPages - 1){
-        beforePage = beforePage - 1
-    } 
     
-
-    if(page == 1){
-        afterPag = afterPag + 2;
-    }
-    else if (page == 2){
-        afterPag = afterPag + 1;
+    } else if (page == totalPages - 1) {
+        beforePage = beforePage - 1;
     }
 
-    for (var plength = beforePage; plength <= afterPag ; plength ++){
-        if (plength > totalPages){
+    if (page == 1) {
+        afterPage = afterPage + 2;
+    
+    } else if (page == 2) {
+        afterPage  = afterPage + 1;
+    }
+    
+    for (var plength = beforePage; plength <= afterPage; plength++) {
+        if (plength > totalPages) { 
             continue;
         }
-        if (plength == 0) { //if plength is 0 than add +1 in plength value
+        
+        if (plength == 0) { 
             plength = plength + 1;
         }
-        if(page = plength){
-            active = 'active'
+        if(page == plength){ 
+            active = "active";
         }
-        else {
-            active = '';
+        else{
+            active = "";
         }
-        liTag  +=  `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`
-    }
-    if(page < totalPages - 1){
-        if(page < totalPages - 2){
-            liTag += `<li class="dots"><span>...</span></li>`
-        }
-        liTag +=`<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`
+        liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
     }
 
-    if (page < totalPages) {
-        liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`
+    if(page < totalPages - 1){ 
+        if(page < totalPages - 2){ 
+            liTag += `<li class="dots"><span>...</span></li>`;
+        }
+        liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
     }
-    element.innerHTML = liTag;
+    if (page < totalPages) { 
+        liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+    }
+    element.innerHTML = liTag; 
     return liTag;
 }
-
-element.innerHTML = createPagination (totalPages, page);
-
- 
