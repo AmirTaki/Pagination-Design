ada81b5a125e298b3dc319ffec79d01ac95dda55
amir <amirtaki623@gmail.com>
2025-10-02 19:35:09 +0330

    part 6

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ const element = document.querySelector('.pagination ul')
 let totalPages = 20;
 let page = 10;
 
+
+createPagination = (totalPages, page) => {
+
+}
+
 element.innerHTML = createPagination (totalPages, page);
+
+ 
