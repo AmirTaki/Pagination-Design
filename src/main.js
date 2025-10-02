a38d701e9fd246309069c7bf0951e7be65583012
amir <amirtaki623@gmail.com>
2025-10-02 19:38:33 +0330

    part 8

--- src/main.js
+++ src/main.js
@@ -9,7 +9,10 @@ const  createPagination = (totalPages, page) => {
     let active;
     let beforePage = page - 1;
     let afterPag = page + 1;
-    
+    if (page > 1){
+        liTag += `<li class = 'btn prev' onclick = "createPagination(totalPages, ${page - 1})">
+        <span><i class = 'fas fa-angle-left'></i>prev</span> </li>` 
+    }
 }
 
 element.innerHTML = createPagination (totalPages, page);
